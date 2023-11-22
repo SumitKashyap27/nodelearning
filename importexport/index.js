62a3ce1f3dd8b3ebbdd1c55f270689360f7ea1d5
SumitKashyap27 <sumit20020927@gmail.com>
2023-11-22 17:27:33 +0530

    commit

--- importexport/index.js
+++ importexport/index.js
@@ -5,3 +5,4 @@ console.log(add(5,5));
 console.log(sub(10,5));
 console.log(mul(10,5));
 
+ 
---
+++ npmMods/index.js
@@ -0,0 +1,4 @@
+const validator= require("validator")
+
+const res=validator.isEmail("sumit@sumit.com");
+console.log(res);
