f8aad893d64e12633aa73aab00de562338f80472
SumitKashyap27 <sumit20020927@gmail.com>
2023-11-24 17:54:45 +0530

    commit

---
+++ ModsWrapperFn/index.js
@@ -0,0 +1,10 @@
+// const name ="sumit";
+// console.log(name);
+
+// what basically a wrapper fn is this above code you 
+// have written is just an java script what main thing
+// work behind is wrapper fn it look like 
+(function (exports,require,module,__filename,__dirname){
+    const a="name";
+    console.log(a);}
+)
